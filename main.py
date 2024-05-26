be3fc393f3dc8c0f6cb4612f2687d50e32c64df5
Anvar1902 <sajarovagt@gmail.com>
2024-05-26 19:31:55 +0300

    .

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start(message: types.Message):
     kb = [
-        [types.KeyboardButton(text="Открыть кликер", web_app=WebAppInfo(url="https://anvar1902.github.io/Web_apps_test_For_UNION"))]
+        [types.InlineKeyboardButton(text="Открыть кликер", web_app=WebAppInfo(url="https://anvar1902.github.io/Web_apps_test_For_UNION"))]
     ]
 
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите действие"
+    keyboard = types.InlineKeyboardMarkup(
+        inline_keyboard=kb
     )
     await message.answer(text="Привет, я бот, который умеет отвечать на вопросы Вроде бы но после того как я написал Вроде бы ещё и с большой буквы уже пишет создатель а не рандом хуитас", reply_markup=keyboard)
 async def main():
