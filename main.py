f270f3d916d5a1a1e75e6c8c97c9d3c4dba82a93
Anvar1902 <sajarovagt@gmail.com>
2024-05-29 11:31:06 +0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.types.web_app_info import WebAppInfo
 from aiogram.filters.command import Command
@@ -18,6 +19,12 @@ async def start(message: types.Message):
         inline_keyboard=kb
     )
     await message.answer(text="Привет, я бот, который умеет отвечать на вопросы Вроде бы но после того как я написал Вроде бы ещё и с большой буквы уже пишет создатель а не рандом хуитас", reply_markup=keyboard)
+
+dp.message(content_types=["web_app_data"])
+async def web_app_update(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(res)
+
 async def main():
     await dp.start_polling(bot)
 
